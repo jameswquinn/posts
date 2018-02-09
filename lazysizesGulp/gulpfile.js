0c7d4264ed64637056a5619b7b9328b84b785b40
jameswquinn <jameswquinn@users.noreply.github.com>
2018-02-09 23:42:17 +0000

    update

--- lazysizesGulp/gulpfile.js
+++ lazysizesGulp/gulpfile.js
@@ -18,9 +18,9 @@ gulp.task('views', function() {
             data_srcset: 'data-srcset',
             /*
             suffix: {
-                '1x': '-320x@1x',
-                '2x': '-320x@2x',
-                '3x': '-320x@3x'
+                '1x': '@1x',
+                '2x': '@2x',
+                '3x': '@3x'
             },
             */
             suffix: {
@@ -62,3 +62,53 @@ gulp.task('service-worker', () => {
     console.warn('Service worker generation failed: ' + error);
   });
 });
+
+
+
+var notify = require("gulp-notify");
+gulp.task('note', () => {
+gulp.src("./src/test.ext")
+  .pipe(notify("Hello Gulp!"))
+});
+
+/*
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+gulp.task('lint', () => {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint'], function () {
+    // This will only run if the lint task is successful...
+});
+
+var csslint = require('gulp-csslint');
+
+gulp.task('css', function() {
+  gulp.src('client/css/*.css')
+    .pipe(csslint())
+    .pipe(csslint.formatter());
+});
+
+var w3cjs = require('gulp-w3cjs');
+
+gulp.task('w3cjs', function () {
+	gulp.src('src/*.html')
+		.pipe(w3cjs())
+		.pipe(w3cjs.reporter());
+});
+*/
